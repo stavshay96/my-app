46ccab0467340df8a47a81fbd74e950d9a245db2
DanielK3005 <danielkaramzin@gmail.com>
2023-04-28 14:20:56 +0300

    signin+signout done

--- src/components/PagesAfterLogin/SignIn.jsx
+++ src/components/PagesAfterLogin/SignIn.jsx
@@ -5,16 +5,15 @@ import "./SignIn.css"
 const SignIn =(props) =>
 {
     const signInHandler = (event) =>{
-        /*let removing = document.cookies.remove({
-            "userID": props.userInfo["userID"],
-            "fullName": props.userInfo["fullName"]
-          });
-          props.userInfo({});*/
+        document.cookie = 'userID=; Max-Age=0; path=/;';
+        document.cookie = 'fullName=; Max-Age=0; path=/;';
+        document.cookie = 'email=; Max-Age=0; path=/;';
+        window.location.reload(true);
     }
 
     return (
         <div>
-            <Button className="btnNamePresent" style={{position:'fixed', top:'5%', right:'9%' ,fontSize:'1.6vw'}}> ! {`${props.userInfo["fullName"]}`} שלום</Button>
+            <h1 className="btnNamePresent" style={{position:'fixed', top:'5%', right:'9%' ,fontSize:'1.6vw'}}> ! {`${props.userInfo["fullName"]}`} שלום</h1>
             <Button className="btnSignIn" style={{position:'fixed', top:'5%', right:'24%'}} onClick={signInHandler}>
                 התנתק
             </Button>
