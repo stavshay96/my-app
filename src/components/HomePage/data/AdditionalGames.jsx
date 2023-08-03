8e4b0ef83bd49711799bf7b393a8fd614748a6cf
Stav Shay <stavsy@mta.ac.il>
2023-08-03 15:35:19 +0300

    popup leagues

---
+++ src/components/HomePage/data/AdditionalGames.jsx
@@ -0,0 +1,18 @@
+const AdditionalGames = [
+    
+    { leagueID: 1,
+        name: "טריוויה",
+        pathName:"TtiviaQuiz"},
+    {leagueID:2,
+        name:"דראפט ליג",
+        pathName:"DraftLeague"},
+    {leagueID: 3,
+        name:"זיהוי שחקן",
+        pathName:"IdentifyPlayer"},
+    {leagueID: 4,
+        name:"זיהוי קבוצה",
+        pathName:"IdentifyTeam"},
+   
+]
+
+export default AdditionalGames; 
---
+++ src/components/HomePage/data/PredictionsLeagues.jsx
@@ -0,0 +1,23 @@
+const PredictionsLeagues = [
+    
+    { leagueID: 1,
+        name: "פרמייר ליג",
+        pathName:"PremierLeague"},
+    {leagueID:2,
+        name:"ליגת העל",
+        pathName:"LigatHa'al"},
+    {leagueID: 3,
+        name:"ליגה לאומית",
+        pathName:"LigaLeumit"},
+    {leagueID: 4,
+        name:"ליגת האלופות",
+        pathName:"ChampionsLeague"},
+    {leagueID: 5,
+        name:"סופרליג",
+        pathName:"SuperLeague"},
+    {leagueID: 6,
+        name:"ניחוש יחיד",
+        pathName:"SinglePrediction"}
+]
+
+export default PredictionsLeagues; 
