89aae7f08bbac8c85d8f20c5478bb09fd204b8be
Stav Shay <stavsy@mta.ac.il>
2023-09-11 18:45:03 +0300

    fixing captain undefined

--- src/components/Fantasy/data/Matches.jsx
+++ src/components/Fantasy/data/Matches.jsx
@@ -1,14 +1,14 @@
 const Matches =[
-    {id:1, HomeTeam: 'וולבס' , ScoreHomeTeam: '-', ScoreAwayTeam: '-', AwayTeam: 'ליברפול'},
-    {id:2, HomeTeam: 'אסטון וילה' , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: 'קריסטל פאלאס'},
-    {id:3, HomeTeam: 'פולהאם' , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: 'לוטון'},
-    {id:4, HomeTeam: "מנצ'סטר יונייטד" , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: 'ברייטון'},
-    {id:5, HomeTeam: 'טוטנהאם' , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: 'שפילד יונייטד'},
-    {id:6, HomeTeam: 'ווסטהאם' , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: "מנצ'סטר סיטי"},
-    {id:7, HomeTeam: 'ניוקאסל' , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: 'ברנטפורד'},
-    {id:8, HomeTeam: "בורנמות" , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: "צ'לסי"},
-    {id:9, HomeTeam: 'אברטון' , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: 'ארסנל'},
-    {id:10, HomeTeam: 'נוטינגהאם' , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: 'ברנלי'},
+    {id:1, HomeTeam: 'Wolverhampton' , ScoreHomeTeam: '-', ScoreAwayTeam: '-', AwayTeam: 'Liverpool'},
+    {id:2, HomeTeam: 'Aston Villa' , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: 'Crystal Palace'},
+    {id:3, HomeTeam: 'Fulham' , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: 'Luton Town'},
+    {id:4, HomeTeam: "Man United" , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: 'Brighton Hove'},
+    {id:5, HomeTeam: 'Tottenham' , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: 'Sheffield Utd'},
+    {id:6, HomeTeam: 'West Ham' , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: "Man City"},
+    {id:7, HomeTeam: 'Newcastle' , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: 'Brentford'},
+    {id:8, HomeTeam: "Bournemouth" , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: "Chelsea"},
+    {id:9, HomeTeam: 'Everton' , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: 'Arsenal'},
+    {id:10, HomeTeam: 'Nottingham' , ScoreHomeTeam:'-', ScoreAwayTeam: '-', AwayTeam: 'Burnley'},
 ]
 
 export default Matches;
